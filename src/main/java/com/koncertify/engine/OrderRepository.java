7e80dd94d1c735a2340d592d6f4ddae91860eeff
Meghna Ravikumar <meghnaravikumar06@gmail.com>
2026-05-21 15:08:56 +0530

    chore: remove duplicate legacy files from root directory

--- src/main/java/com/koncertify/engine/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.koncertify.engine;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Inherits built-in save capabilities for our confirmation records
-}
